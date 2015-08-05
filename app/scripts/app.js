6b61826fffffd771b48df24e08dc4feaad3668f7
David Pham <davidpham5@gmail.com>
2015-08-05 16:40:13 -0400

    experimenting with bower path

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -23,12 +23,12 @@ angular
 	
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
+        templateUrl: 'app/views/main.html',
         controller: 'MainCtrl',
         controllerAs: 'main'
       })
       .when('/about', {
-        templateUrl: 'views/about.html',
+        templateUrl: 'app/views/about.html',
         controller: 'AboutCtrl',
         controllerAs: 'about'
       })
