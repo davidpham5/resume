4e7147ab5bd9db6f558ce19e29595fcc630fff14
David Pham <davidpham5@gmail.com>
2015-08-05 16:35:30 -0400

    experimenting with bower path

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -20,7 +20,7 @@ angular
     'ngTouch'
   ])
   .config(function ($locationProvider, $routeProvider) {
-	$locationProvider.html5Mode(true);
+	
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
