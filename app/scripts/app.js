6d621268c53d8c84538510305ae4f9a6ccff6d72
David Pham <davidpham5@gmail.com>
2017-03-09 00:12:06 -0500

    publish to gh-pages

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -23,17 +23,17 @@ angular
 
     $routeProvider
       .when('/', {
-	  	  templateUrl: 'views/main.html',
+	  	  templateUrl: 'app/views/main.html',
         controller: 'MainCtrl',
         controllerAs: 'main'
       })
       .when('/about', {
- 	      templateUrl: 'views/about.html',
+ 	      templateUrl: 'app/views/about.html',
         controller: 'AboutCtrl',
         controllerAs: 'about'
       })
       .when('/symplicity', {
-        templateUrl: 'views/symplicity-detail.html',
+        templateUrl: 'app/views/symplicity-detail.html',
         controller: 'SymplicityCtrl',
         controllerAs: 'symp'
       })
