54cc50a8f351fba01435c34e8f0832c8663194bc
David Pham <davidpham5@gmail.com>
2019-12-23 22:41:30 -0500

    add new build

--- docs/_next/static/runtime/webpack.js
+++ docs/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c6b98acaf0d4855c4dcb";
+/******/ 	var hotCurrentHash = "d0d656bc031d4a979ff8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
