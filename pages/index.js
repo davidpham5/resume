7d53759cd9c50e5138a5c321edde14b7fa20aae0
David Pham <davidpham5@gmail.com>
2019-12-23 23:13:47 -0500

    replace index for resume page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,40 @@
-import React from 'react';
-import Welcome from '../components/Welcome';
+// import React from 'react';
+// import Welcome from '../components/Welcome';
 
-function Home() {
+// function Home() {
+//   return (
+//     <div>
+//       <Welcome />
+//     </div>
+//   )
+// }
+
+// export default Home
+import React from 'react';
+import WorkExp from '../components/Work-Experience/WorkExp'
+import WorkFlow from '../components/WorkFlow'
+import Hero from '../components/Hero'
+import Education from '../components/Education'
+import Skills from '../components/Skills'
+import Organizations from '../components/Organizations'
+
+const Resume = () => {
   return (
     <div>
-      <Welcome />
+      <Hero className="flex mb-5" />
+      <div className="flex mb-4 container md:p-12">
+        <div className="w-2/3">
+          <WorkExp />
+          <WorkFlow />
+        </div>
+        <div className="w-1/3">
+          <Education />
+          <Skills />
+          <Organizations />
+        </div>
+      </div>
     </div>
   )
 }
 
-export default Home
+export default Resume;
